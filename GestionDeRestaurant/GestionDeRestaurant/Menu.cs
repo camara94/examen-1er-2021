91e573431b20820ffc94886462f67d65ee1aa1ac
Laby Damaro Camara <ldamaro98@gmail.com>
2021-12-15 05:27:14 +0100

    classe Menu

---
+++ GestionDeRestaurant/GestionDeRestaurant/Menu.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionDeRestaurant
+{
+    class Menu : Repas
+    {
+        private String nomBoisson;
+        private List<Plat> plats;
+
+        public Menu(String nom, double prix, bool estService, string nomBoisson, List<Plat> plats) : base(nom, prix, estService)
+        {
+            this.nomBoisson = nomBoisson;
+            this.Plats = plats;
+        }
+
+        public string NomBoisson { get => nomBoisson; set => nomBoisson = value; }
+        internal List<Plat> Plats { get => plats; set => plats = value; }
+
+        public override void ServirRepas()
+        {
+            this.estService = true;
+        }
+
+        override
+        public String ToString()
+        {
+            base.ToString();
+            String message = "";
+            foreach (Repas r in this.plats)
+            {
+                message += r.ToString() + "\n";
+            }
+
+            message += " la boisson est : " + this.nomBoisson;
+
+            return message;
+        }
+    }
+}
