c8e1f10c8a7c1dac085dcb89cc99f2497bebee0d
Laby Damaro Camara <ldamaro98@gmail.com>
2021-12-15 05:26:22 +0100

    la classe Plat

---
+++ GestionDeRestaurant/GestionDeRestaurant/Plat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionDeRestaurant
+{
+    class Plat: Repas
+    {
+        private String description;
+       
+
+        public Plat(String nom, double prix, bool estService, string description): base(nom, prix, estService)
+        {
+            this.description = description;
+        }
+
+        public string Description { get => description; set => description = value; }
+
+        public override void ServirRepas()
+        {
+            this.estService = true;
+        }
+
+        override
+        public String ToString()
+        {
+            base.ToString();
+            return " la description est : " + this.description;
+        }
+
+    }
+}
